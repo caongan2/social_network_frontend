36eb145a7ca9c1ea0e9fff2dfe127035e4380f34
cuongpham270695 <cuong.pham270695@gmail.com>
2021-09-07 21:25:05 +0700

    cuong-user-profile-done-v2

--- src/app/components/users/user-navbar/user-navbar.component.ts
+++ src/app/components/users/user-navbar/user-navbar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from "../../../services/user.service";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-user-navbar',
@@ -9,15 +10,27 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class UserNavbarComponent implements OnInit {
 
-  user:any;
-  // @ts-ignore
-  id = +this.activatedRoute.snapshot.paramMap.get('id')
-  constructor(private userService: UserService,
-              private activatedRoute: ActivatedRoute) { }
+  user: any
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private userService: UserService) { }
 
   ngOnInit(): void {
-    this.userService.getById().subscribe(res => {
-      this.user = res;
+    this.user = JSON.parse(<string>this.authService.getUser());
+    // this.userService.userCast.subscribe(user => this.user = user);
+  }
+
+
+  logout() {
+    this.authService.logout().subscribe(res => {
+      localStorage.clear()
+      this.router.navigate(['']).then(r => {
+        console.log(res)
+        // console.log('Success')
+      }).catch(error => {
+        console.log("Logout error")
+      })
     })
   }
 
