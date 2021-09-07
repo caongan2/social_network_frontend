bac4c88934f69e82499c0ff140525532d2d65447
Ninh02 <ninhdo10021998@gmail.com>
2021-09-07 17:28:23 +0700

    review_like

--- src/app/components/post/post-list/post-list.component.ts
+++ src/app/components/post/post-list/post-list.component.ts
@@ -16,7 +16,7 @@ export class PostListComponent implements OnInit {
   user: any;
   isLike: boolean = false;
   formComment: FormGroup | undefined
-  post: any
+  post: any;
   showComment: boolean = false
 
   constructor(private postService: PostService,
